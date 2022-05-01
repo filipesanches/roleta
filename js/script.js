c2047ddd1cc8b3930e57b82a18d57aef0f000441
Filipe Sanches <filipepereirasanches@gmail.com>
2022-05-01 19:31:19 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function myFunction() {
     x.className = "nav";
   }
 }
+window.addEventListener("load",function(){
+  const dia = document.getElementById("dia");
+  const mes = document.getElementById("mes");
+  const mesNome = ["janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro"]
+  let data = new Date();
+  const diaFormatado = data.getDate() < 10 ?  "0" + data.getDate() :  data.getDate();
+  dia.innerHTML = diaFormatado;
+  mes.innerHTML = mesNome[data.getMonth()]
+})
